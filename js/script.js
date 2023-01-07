6468ce10dd23cd5f488c24c94acc115bccc85e01
Anisa Haxhijaha <anisahjaha@hotmail.com>
2023-01-07 16:27:13 +0100

    Added the menu toggle for an easier form to make the navbar responsive.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ window.addEventListener('scroll', function(){
     const header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
 });
+function toggleMenu(){
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+
+}
+
 //Register Modal
 const modal = document.querySelector('.modal');
 const closeModalBtn = document.querySelector('.close-modal');
